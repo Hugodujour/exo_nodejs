f013f71bd8ebebc1207df8b0e1e8291fec30b511
Hugodujour <hugogreg@live.fr>
2024-08-10 19:49:23 +0200

    controller

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -3,24 +3,13 @@ const UnauthorizedError = require("../../errors/unauthorized");
 const jwt = require("jsonwebtoken");
 const config = require("../../config");
 const usersService = require("./users.service");
+const articlesService = require("../articles/articles.service");
 
 class UsersController {
   async getAll(req, res, next) {
     try {
       const users = await usersService.getAll();
-      res.status(200).json(users);
-    } catch (err) {
-      next(err);
-    }
-  }
-  async me(req, res, next) {
-    try {
-      const me = req.user.id;
-      const user = usersService.get(me);
-      if (!user) {
-        throw new NotFoundError();
-      }
-      res.json(user);
+      res.json(users);
     } catch (err) {
       next(err);
     }
@@ -29,6 +18,27 @@ class UsersController {
     try {
       const id = req.params.id;
       const user = await usersService.get(id);
+      if (!user) {
+        throw new NotFoundError();
+      }
+      res.json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+  async getUserArticles(req, res, next) {
+    try {
+      const userId = req.params.userId;
+      const articles = await articlesService.getArticlesByUser(userId);
+      res.json(articles);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async me(req, res, next) {
+    try {
+      const user = req.user;
       if (!user) {
         throw new NotFoundError();
       }
@@ -71,11 +81,11 @@ class UsersController {
   async login(req, res, next) {
     try {
       const { email, password } = req.body;
-      const userId = await usersService.checkPasswordUser(email, password);
-      if (!userId) {
+      const User = await usersService.checkPasswordUser(email, password);
+      if (!User) {
         throw new UnauthorizedError();
       }
-      const token = jwt.sign({ userId }, config.secretJwtToken, {
+      const token = jwt.sign(User, config.secretJwtToken, {
         expiresIn: "3d",
       });
       res.json({
