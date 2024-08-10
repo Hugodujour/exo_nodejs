23551bafd4579e0774108f99a8d654b058a7737e
Hugodujour <hugogreg@live.fr>
2024-08-10 20:58:58 +0200

    logs pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: "app",
-      script: "./www/app.js",
+      name: "my-app",
+      script: "./server.js",
+      instances: 3,
+      exec_mode: "cluster",
+      max_memory_restart: "200M",
+      error_file: "./logs/error_file.log",
+      out_file: "./logs/out_file.log",
+      log_file: "./logs/log_file.log",
+      combine_logs: true,
+      env: {
+        NODE_ENV: "development",
+      },
       env_production: {
         NODE_ENV: "production",
       },
