b226e9976b427f60fcf96b3c37a4a0fabffcb1ea
Hugodujour <hugogreg@live.fr>
2024-08-10 21:00:02 +0200

    correction log

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       instances: 3,
       exec_mode: "cluster",
       max_memory_restart: "200M",
-      error_file: "./logs/error_file.log",
+      error_file: "./logs/err.log",
       out_file: "./logs/out_file.log",
       log_file: "./logs/log_file.log",
       combine_logs: true,
